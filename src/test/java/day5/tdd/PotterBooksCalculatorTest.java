6c8ce015a7ccff983a045dd9bd75462ac5b6c004
Hiroshige Murakawa <murakawa@comsoft.co.jp>
2025-08-05 14:20:43 +0900

    [test]異なる巻を6冊以上購入したら合計金額が25%オフとなるテスト

--- src/test/java/day5/tdd/PotterBooksCalculatorTest.java
+++ src/test/java/day5/tdd/PotterBooksCalculatorTest.java
@@ -108,7 +108,11 @@ class PotterBooksCalculatorTest {
         // 異なる巻5冊 (25%オフ)
         arguments(Arrays.asList(1, 2, 3, 4, 5), BOOK_PRICE * 5 * 0.75),
         arguments(Arrays.asList(2, 1, 3, 4, 5), BOOK_PRICE * 5 * 0.75), // (順序が異なっても結果は同じはず)
-        arguments(Arrays.asList(3, 3, 3, 1, 1), BOOK_PRICE * 5 * 0.75)
+        arguments(Arrays.asList(3, 3, 3, 1, 1), BOOK_PRICE * 5 * 0.75),
+        // 異なる巻6冊以上 (25%オフ)
+        arguments(Arrays.asList(1, 2, 3, 4, 5 ,1), BOOK_PRICE * 6 * 0.75),
+        arguments(Arrays.asList(2, 1, 3, 4, 5, 1), BOOK_PRICE * 6 * 0.75), // (順序が異なっても結果は同じはず)
+        arguments(Arrays.asList(3, 3, 3, 1, 1, 1, 3), BOOK_PRICE * 7 * 0.75)
       );
     }
   }
